eccce96859bc1f06368c10627053084f607c6056
deveshagnihotri <djdevesh524@gmail.com>
2020-05-17 17:22:57 +0530

    find permutation in array

--- array/findDuplicates.js
+++ array/findDuplicates.js
@@ -1,3 +1,5 @@
+//Given a read only array of n + 1 integers between 1 and n, find one number that repeats in linear time using less than O(n) space and traversing the stream sequentially O(1) times.
+
 const findDuplicate = (arr) => {
   let result = {};
 
---
+++ array/findPermutation.js
@@ -0,0 +1,15 @@
+const permutations = (arr) => {
+  if (arr.length <= 2) return arr.length === 2 ? [arr, [arr[1], arr[0]]] : arr;
+  return arr.reduce(
+    (acc, item, i) =>
+      acc.concat(
+        permutations([...arr.slice(0, i), ...arr.slice(i + 1)]).map((val) => [
+          item,
+          ...val,
+        ])
+      ),
+    []
+  );
+};
+let result = permutations([1, 2, 3]);
+console.log(result);
